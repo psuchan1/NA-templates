70a3aa0a1cef3e052d1f6c942e66590ef2f9ed86
Pawel Suchan <pawel.suchan.ext@nokia.com>
2016-11-10 15:56:38 +0100

    All test in 30 directory pass

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -1,5 +1,36 @@
 #pragma once
 
+#include <type_traits> 
+
+/*
 template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of;
+struct is_any_of: std::false_type
+{
+};
+
+template<typename T, typename T2, typename T3>
+struct is_any_of<T,T,T2,T3>: std::true_type
+{
+};
+
+template<typename T, typename T2, typename T3>
+struct is_any_of<T,T2,T,T3>: std::true_type
+{
+};
+
+
+template<typename T, typename T1, typename T2>
+struct is_any_of<T,T1,T2,T>: std::true_type
+{
+};
+*/
+
+template<typename T, typename T1, typename T2, typename T3>
+struct is_any_of : std::disjunction<std::is_same<T,T1>, std::is_same<T,T2>
+, std::is_same<T,T3>>
+{
+
+}; 
+
+
 
