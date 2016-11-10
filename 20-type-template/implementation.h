58686e06eed013871c241dbc4be9277072026f75
Pawel Suchan <pawel.suchan.ext@nokia.com>
2016-11-10 14:51:56 +0100

    In 20 direcotry all test pass

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -2,12 +2,14 @@
 
 #include <cstddef>
 #include <utility>
+#include <boost/optional.hpp>
 
 template<typename T>
 class container_wrapper
 {
 	public:
 	std::size_t size() const {return container.size();} 
+
 	container_wrapper() = default;
 	container_wrapper(const container_wrapper &) = default;
 	container_wrapper(container_wrapper &&) = default;
@@ -17,11 +19,29 @@ class container_wrapper
 	container_wrapper(T v)
 	: container(std::move(v)) {};
 
-
-
 	private:
 	T container;
 
+};
+
+
+template<typename T>
+class container_wrapper<boost::optional<T>>
+{
+	public:
+	std::size_t size() const {return container ? 1: 0;} 
+
+	container_wrapper() = default;
+	container_wrapper(const container_wrapper &) = default;
+	container_wrapper(container_wrapper &&) = default;
+	container_wrapper& operator=(const container_wrapper &) = default;	
+	container_wrapper& operator=(container_wrapper &&) = default;	
+
+	container_wrapper(boost::optional<T> v)
+	: container(std::move(v)) {};
+
+	private:
+	boost::optional<T> container;
 
 };
 
