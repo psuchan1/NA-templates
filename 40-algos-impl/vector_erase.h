e13e1506ca0d71214e649dd67e264a4b8cfe3f2c
Pawel Suchan <pawel.suchan.ext@nokia.com>
2016-11-10 18:59:45 +0100

    All test in 40 directory pass

--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,23 @@
 #pragma once
 
 // define vector_erase here
+#include "find_if.h"
+#include <vector>
+template<typename T>
+void vector_erase(T & vector, const typename T::value_type & value )
+{
+	vector.erase(std::remove(vector.begin(), vector.end(), value), vector.end());
+	
+}
 
+
+template<typename T, typename Alloc>
+void vector_erase(T vector, int n )
+{
+	int size = vector.size();
+	for(int i = 0; i < size; i++)
+	{ 		
+		auto it = find_if(vector.begin(), vector.end(), [&n](auto elem){ return elem == n;});
+		if(it != vector.end()) vector.erase(it);
+	}
+}
